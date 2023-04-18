6049f70f6ba7b26c97c050ab87bf4cafc5c848b1
JOKI6696 <J.GONZALESRAV@GMAIL.COM>
2023-04-18 12:19:12 -0500

    Se hizo el ruteado del componente login y menu. Se solucionaron problemas con bootstrap

--- burguer-queen/src/app/app.module.ts
+++ burguer-queen/src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
