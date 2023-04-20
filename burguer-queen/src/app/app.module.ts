27064beb7df211a0ced11b5c8f1bc2b150845dbd
JOKI6696 <J.GONZALESRAV@GMAIL.COM>
2023-04-20 10:11:57 -0500

    Componente Login: configuración de formulario reactivo y validaciones

--- burguer-queen/src/app/app.module.ts
+++ burguer-queen/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { MenuComponent } from './menu/menu.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { MenuComponent } from './menu/menu.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
